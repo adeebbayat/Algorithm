2907e416a6543a1482673cafad4157375eb6f7ff
Adeeb Bayat <adeebbayat@yahoo.com>
2023-09-14 08:34:31 -0700

    Initial Commit

---
+++ W7D3.js
@@ -0,0 +1,39 @@
+const nums1 = [1, 1, 1, 1];
+const expected1 = [1];
+
+const nums2 = [1, 1, 2, 2, 3, 3];
+const expected2 = [1, 2, 3];
+
+const nums3 = [1, 1, 2, 3, 3, 4];
+const expected3 = [1, 2, 3, 4];
+
+const nums4 = [1, 1];
+const expected4 = [1];
+
+/**
+ * De-dupes the given sorted array.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Array<number>} nums
+ * @returns {Array<number>} The given array deduped.
+ */
+function dedupeSorted(nums) {
+    indexes = []
+    temp = []
+    for(var i = 0; i < 10; i++){
+        nums.forEach((element, index) => {
+            if (element === i) {
+            indexes.push(index);
+            }
+        })
+    if (indexes.length > 0){
+        temp.push(i)
+    }
+    indexes = []
+}
+return temp
+}
+console.log(dedupeSorted(nums1))
+console.log(dedupeSorted(nums2))
+console.log(dedupeSorted(nums3))
+console.log(dedupeSorted(nums4))
