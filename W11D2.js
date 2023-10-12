492451355c85761fadb2db125de866a2d6dde9ef
Adeeb Bayat <adeebbayat@yahoo.com>
2023-10-12 08:37:09 -0700

    Initial Commit

--- W11D2.js
+++ W11D2.js
@@ -6,17 +6,6 @@
 */
 
 // given searchFor and items
-const items = [
-    { firstName: "Bob", lastName: "Robert", age: 31 },
-    { firstName: "John", lastName: "Smith", age: 25 },
-    { firstName: "Bob", lastName: "White", age: 31 },
-    { firstName: "Bob", lastName: "Smith", age: 27 },
-];
-// db.ninjas.find({firstName: "Bob", age: 31})
-const searchFor1 = {
-    firstName: "Bob",
-    age: 31
-};
 // return a new list of objects containing the same key pair values
 const output1 = [
     { firstName: "Bob", lastName: "Robert", age: 31 },
@@ -36,6 +25,17 @@ function findObjectsFilter(searchObj, items) {
     // PSEUDOCODE!!!!
 }
 
+const items = [
+    { firstName: "Bob", lastName: "Robert", age: 31 },
+    { firstName: "John", lastName: "Smith", age: 25 },
+    { firstName: "Bob", lastName: "White", age: 31 },
+    { firstName: "Bob", lastName: "Smith", age: 27 },
+];
+// db.ninjas.find({firstName: "Bob", age: 31})
+const searchFor1 = {
+    firstName: "Bob",
+    age: 31
+};
 
 function findObjectsFilter(searchObj, items) {
     const result = []; //initialize an empty array to store the matching objects
@@ -43,9 +43,11 @@ function findObjectsFilter(searchObj, items) {
     //iterate through each item in the "items" array
   for (const item of items) {
       //assume that the item matches the search object to begin with
+      console.log(item)
       let isMatch = true;
       //iterate through each key in the searchObj
       for (const key in searchObj) {
+        console.log(key)
         //check if the item has the same key and the same value as in the searchobj
       if (item[key] !== searchObj[key]) {
           // if any key-value pair doesn't match, set isMatch to false and break loop
---
+++ W11D4.js
@@ -0,0 +1,82 @@
+/* 
+  findByIdAndUpdate(id, updateObject, arr)
+  Given an id, an object that has keys with corresponding updated values, and an array of objects
+  Find the object by "id" key that matches the given id value and then update that object's
+  keys with the provided new values.
+  Return the updated object, or null if no object was found
+
+  check
+  someObj.hasOwnProperty("key")
+  obj[key] === undefined
+  Object.keys(someObj) // returns [an array of "keys"]
+*/
+
+const students = [
+    {
+      id: 1,
+      name: "student1",
+      isLateToday: false,
+      lateCount: 15,
+      redBeltStatus: false
+    },
+    {
+      id: 2,
+      name: "student2",
+      isLateToday: false,
+      lateCount: 1,
+      redBeltStatus: false
+    },
+    {
+      id: 3,
+      name: "student3",
+      isLateToday: false,
+      lateCount: 0,
+      redBeltStatus: false
+    }
+  ];
+  
+  // Ninja.findByIdAndUpdate(req.params._id, {updateObject})
+  /*
+    something
+    here
+  */
+
+    /**
+   * Finds the specified obj by id and updates it with the given key value pairs.
+   * - Time: O(?).
+   * - Space: O(?).
+   * @param {number} id
+   * @param {Object} updatedValues Key value pairs used to update the found obj.
+   * @param {Array<Object>} collection
+   * @returns {?Object} The object that was updated or null if no object found.
+   */
+
+
+function findByIdAndUpdate(id, updatedValues, collection) { 
+    // Check if the given ID is within the valid range of collection indices
+    if(id <= collection.length){
+      // Extract the keys and values from the updatedValues object
+    let keys = Object.keys(updatedValues)
+    let values = Object.values(updatedValues)
+      // Iterate through the keys and values
+    for(let i = 0; i < keys.length; i++){
+        // Check if the collection object at the specified index (id-1) has the property (key)
+        if(collection[id-1].hasOwnProperty(keys[i])){
+          // Update the property with the new value
+        collection[id-1][keys[i]] = values[i]
+        }
+    }
+    
+      // Return the updated object from the collection
+    return collection[id-1]
+    }
+    
+    // If the ID is out of range, return null
+    return null
+}
+
+
+
+console.log(findByIdAndUpdate(3, { redBeltStatus: true }, students));
+console.log(findByIdAndUpdate(1, { isLateToday: true, lateCount: 16, randomKey: "randomValue" }, students));
+console.log(findByIdAndUpdate(5, {}, students));
